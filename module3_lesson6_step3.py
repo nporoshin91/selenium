353a10074f422b4db2f5fe47d0a242f4a9267dff
Mikalai Paroshyn <mikalai.paroshyn@pandadoc.com>
2021-09-24 17:53:44 +0300

    completed module 3

--- module3_lesson6_step3.py
+++ module3_lesson6_step3.py
@@ -44,10 +44,9 @@ def test_guest_should_see_login_link(browser, link):
         ec.visibility_of_element_located((By.CSS_SELECTOR, ".textarea"))).send_keys(str(math.log(int(time.time()))))
     browser.find_element_by_css_selector(".submit-submission").click()
     try:
-        WebDriverWait(browser, 2).until(
+        assert WebDriverWait(browser, 3).until(
             ec.text_to_be_present_in_element((By.CSS_SELECTOR, ".smart-hints__hint"), "Correct!"))
-    except TimeoutException:
-        incorrect_text = browser.find_element_by_css_selector(".smart-hints__hint").text
+    except:
         global sentence
-        sentence += incorrect_text
+        sentence += browser.find_element_by_css_selector(".smart-hints__hint").text
         print(f"\n{sentence}")
